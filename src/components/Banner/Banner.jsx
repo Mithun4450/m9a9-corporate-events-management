ff9f1bb7b5e96677a248f2f4c337949c80f4065d
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2023-10-07 16:56:01 +0600

    Banner part and footer part have been completed

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import banner from "../../assets/banner.png"
+
+const Banner = () => {
+    return (
+        <div >
+            <img className="h-[80vh] w-full" src={banner} alt="" />
+            
+        </div>
+    );
+};
+
+export default Banner;
